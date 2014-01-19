9eabb6861e534166df72b68a10cc544de7f2f2b8
hezt95 <hezt95@gmail.com>
2014-01-19 15:27:35 +0800

    .

--- find(2nd mission)/findhzt.py
+++ find(2nd mission)/findhzt.py
@@ -9,45 +9,45 @@ def usage():
     print """
 Usage:
 	./findhe.py [--exec command] /path/to/directory/ [regEx]
-""";
-files = [];
+"""
+files = []
 try:
-	execCommand = None;
-	opts,args = getopt.getopt(sys.argv[1:], "", ["exec="]);
+	execCommand = None
+	opts,args = getopt.getopt(sys.argv[1:], "", ["exec="])
 	for opt, arg in opts:
 			if opt in ["-e", "--exec"]:
-				execCommand = arg;
+				execCommand = arg
 			else:
-				usage();
-				sys.exit();
+				usage()
+				sys.exit()
 	if len(args) > 2:
-		usage();
-		sys.exit();
+		usage()
+		sys.exit()
 	elif len(args) == 2:
-		path = args[0];
-		regEx = re.compile( args[1][1:-1] + "$" );
+		path = args[0]
+		regEx = re.compile( args[1][1:-1] + "$" )
 		#print path
 		print regEx
 	elif len(args)==1:
 		if re.search("[.*]",args[0]):
-			usage();
-			sys.exit();
+			usage()
+			sys.exit()
 		else:
-			path = args[0];
-			regEx = re.compile(".+");
+			path = args[0]
+			regEx = re.compile(".+")
 	for dirpath, dirnames, filenames in os.walk(path):
 		for filename in filenames:
 			if re.search(regEx,filename):
-				filepath = os.path.join(dirpath, filename);
+				filepath = os.path.join(dirpath, filename)
 				if execCommand is not None:
-					os.system(execCommand + " \"" + filepath + "\"");
-				files.append(filepath);
+					os.system(execCommand + " \"" + filepath + "\"")
+				files.append(filepath)
 	if len(files) == 0:
 		print 'Noting'
 	for items in files:
 		print items
 
 except:
-	usage();
-	sys.exit();
+	usage()
+	sys.exit()
 
